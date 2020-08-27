664093003b29b555659dc4b7b105c6633d915522
Davi Athanazio Quental <quentaldavi@gmail.com>
2020-08-26 22:27:04 -0300

    ✅ Add Description Component

---
+++ src/components/DescriptionQD/index.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const DescriptionQD = styled.p`
+    font-size: 18rem;
+    color: var(--color-light-gray);
+    font-weight: 300;
+`;
+
+export default DescriptionQD;
