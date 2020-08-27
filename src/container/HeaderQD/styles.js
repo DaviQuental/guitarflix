aa1d03e62400947be9bb417937f185e9efa95ba3
Davi Athanazio Quental <quentaldavi@gmail.com>
2020-08-26 22:28:55 -0300

    ✅ Add Header Container

---
+++ src/container/HeaderQD/styles.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+import ButtonQD from '../../components/ButtonQD/index';
+import container from '../../styles/tools/container';
+
+export const Header = styled.header`
+    background-color: #000000;
+    padding: 15rem 40rem;
+    border-bottom: 3px solid var(--color-medium-blue);
+
+    @media(max-width: 800px){
+        padding: 15rem 16rem;
+    }
+`;
+
+export const Wrapper = styled.div`
+    ${container};
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    @media screen and (max-width: 800px){
+        & {
+            justify-content: center;
+        }
+
+        & > ${ButtonQD} {
+            background-color: var(--color-medium-blue);
+            border-radius: 0;
+            border: none;
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            width: 100vw;
+        }
+    }
+`;
+
