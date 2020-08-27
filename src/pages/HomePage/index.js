8bc16042498c06e9b6707d88363abed092b9dd57
Davi Athanazio Quental <quentaldavi@gmail.com>
2020-08-26 22:29:19 -0300

    🚧 Add HomePage

---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import HeaderQD from '../../container/HeaderQD';
+import FooterQD from '../../container/FooterQD';
+import MainQD from '../../container/MainQD';
+
+const HomePage = () => (
+    <>
+        <HeaderQD />
+        <MainQD />
+        <FooterQD />
+    </>
+);
+
+export default HomePage;
