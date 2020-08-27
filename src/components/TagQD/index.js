45f306dc4784464f0e426c2106d991255009f9fd
Davi Athanazio Quental <quentaldavi@gmail.com>
2020-08-26 22:27:44 -0300

    ✅ Add Tag Component

---
+++ src/components/TagQD/index.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const TagQD = styled.h2`
+    display: inline-block;
+    color: var(--color-light-gray);
+    font-size: 50rem;
+    padding: 20rem 18rem;
+    border-radius: 4rem;
+    background-color: var(--color-light-blue);
+`;
+
+export default TagQD;
