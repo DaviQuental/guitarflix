0cd486f520c0e57437def138240af52d2f5800f6
Davi Athanazio Quental <quentaldavi@gmail.com>
2020-08-26 22:26:42 -0300

    ✅ Add Button Component

---
+++ src/components/ButtonQD/index.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const ButtonQD = styled.button`
+    padding: 18rem 40rem;
+    background-color: var(--dark-color);
+    border: 1rem solid var(--light-color);
+    border-radius: 5rem;
+    color: var(--light-color);
+
+    &:hover {
+        cursor: pointer;
+    }
+`;
+
+export default ButtonQD;
