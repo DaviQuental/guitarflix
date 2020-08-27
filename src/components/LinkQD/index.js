14e2d16a39b0e38af12df6c513aab1977c2f847f
Davi Athanazio Quental <quentaldavi@gmail.com>
2020-08-26 22:27:25 -0300

    ✅ Add Link Component

---
+++ src/components/LinkQD/index.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const LinkQD = styled.a`
+    cursor: pointer;
+    color: var(--color-medium-blue);
+    text-decoration: none;
+`;
+
+export default LinkQD;
