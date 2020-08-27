5a92b2636897ade52738ad028b4347e6fdb9bab2
Davi Athanazio Quental <quentaldavi@gmail.com>
2020-08-26 22:27:37 -0300

    ✅ Add Logo Component

---
+++ src/components/LogoQD/index.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+import logo from '../../assets/img/logoquental.png'
+
+const LogoQD = styled.img.attrs({ src: logo, alt: 'Logo QuentalDev' })`
+    height: 40rem;
+`;
+
+export default LogoQD;
