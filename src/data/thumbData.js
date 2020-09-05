32e06405899ecf2a10dcba003f8c2dc38dedadfa
Davi Athanazio Quental <quentaldavi@gmail.com>
2020-09-05 13:17:15 -0300

    ✅Add data to thumbData

--- src/data/thumbData.js
+++ src/data/thumbData.js
@@ -1,10 +1,18 @@
 /* ASSETS */
 import neon from '../assets/img/john-neon.jpg';
 import cifraclub from '../assets/img/cifraclub.jpg';
+import tempolegiao from '../assets/img/tempo-legiao.jpg';
 
 export const mainThumb = { src: neon, alt: 'Foto de capa: John Mayer - Neon', imageChannel: cifraclub, altChannel: 'Cifraclub', title: 'Neon - John Mayer (aula de violão)', href: 'https://www.youtube.com/watch?v=sf17osuWTnA' }
 
-export const thumbData = [
+export const learnMusic = [
+    { src: neon, alt: 'Foto de capa: John Mayer - Neon', imageChannel: cifraclub, altChannel: 'Cifraclub', title: 'Neon - John Mayer (aula de violão)', href: 'https://www.youtube.com/watch?v=sf17osuWTnA' },
+    { src: neon, alt: 'Foto de capa: John Mayer - Neon', imageChannel: cifraclub, altChannel: 'Cifraclub', title: 'Neon - John Mayer (aula de violão)', href: 'https://www.youtube.com/watch?v=sf17osuWTnA' },
+    { src: neon, alt: 'Foto de capa: John Mayer - Neon', imageChannel: cifraclub, altChannel: 'Cifraclub', title: 'Neon - John Mayer (aula de violão)', href: 'https://www.youtube.com/watch?v=sf17osuWTnA' },
+    { src: neon, alt: 'Foto de capa: John Mayer - Neon', imageChannel: cifraclub, altChannel: 'Cifraclub', title: 'Neon - John Mayer (aula de violão)', href: 'https://www.youtube.com/watch?v=sf17osuWTnA' },
+];
 
+export const learnedMusic = [
+    { src: tempolegiao, alt: 'Foto de capa: Legiao Urbana - Tempo Perdido', imageChannel: cifraclub, altChannel: 'Cifraclub', title: 'Tempo Perdido - Legião Urbana (aula completa)', href: 'https://www.youtube.com/watch?v=Pt9elq3DYNM' },
 ];
 
