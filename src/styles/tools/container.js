d990e5e64fcd3e9becff88d4866f41f5ef0084d8
Davi Athanazio Quental <quentaldavi@gmail.com>
2020-09-14 15:06:05 -0300

    ✅  Add Media to Container

--- src/styles/tools/container.js
+++ src/styles/tools/container.js
@@ -2,6 +2,11 @@ const container = `
     max-width: 1360px;
     margin-right: auto;
     margin-left: auto;
+
+    @media(max-width: 1360) {
+        padding-left: 16rem;
+        padding-right: 16rem;
+    }
 `;
 
 export default container;
