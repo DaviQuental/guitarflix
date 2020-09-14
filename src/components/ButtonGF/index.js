ff1d0006d8e2c36619ff17e988bc2f3dcd95ca31
Davi Athanazio Quental <quentaldavi@gmail.com>
2020-09-14 15:05:00 -0300

    ⚡️ Improve ButtonGF Effect

--- src/components/ButtonGF/index.js
+++ src/components/ButtonGF/index.js
@@ -6,9 +6,18 @@ const ButtonGF = styled.button`
   border: 1rem solid var(--light-color);
   border-radius: 5rem;
   color: var(--light-color);
+  transition: background-color 200ms linear,
+    transform 200ms cubic-bezier(0, 0, 0.48, 2.2);
 
   &:hover {
+    background-color: var(--color-medium-blue);
+    transform: scale(1.1);
     cursor: pointer;
+  }
+
+  &:active {
+    transition: background-color 200ms linear, transform 100ms linear;
+    transform: scale(0.95);
   }
 `;
 
